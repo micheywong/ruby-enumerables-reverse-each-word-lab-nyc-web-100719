7b78120440e320a761c11d42fb0b32698b571100
Michelle Wong <michellekwong2@gmail.com>
2019-09-26 17:55:06 +0000

    Done.

---
+++ reverse_each_word.rb
@@ -0,0 +1,9 @@
+require 'pry'
+def reverse_each_word(sentence1)
+  new_array = sentence1.split(" ")
+  
+  sentence2 = new_array.collect do |string|
+    string.reverse
+  end
+  sentence2.join(" ")
+end
